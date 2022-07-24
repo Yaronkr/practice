3964dcf022b1e539268af7ffa688eabb282cde0d
Yaron Kr <65701682+Yaronkr@users.noreply.github.com>
2022-07-25 09:37:26 +1000

    adding questions

--- lib/main.dart
+++ lib/main.dart
@@ -1,66 +1,58 @@
+// ignore_for_file: deprecated_member_use, prefer_const_constructors
+
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData.dark(),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: Scaffold(
+        appBar: AppBar(
+          leading: CircleAvatar(
+            radius: 2,
+            backgroundColor: Colors.red[200],
+            
+          ),
+          title: const Text('Title'),
+        ),
+        body: AppBody(),
+      ),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  final String title;
-
+class AppBody extends StatefulWidget {
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<AppBody> createState() => _AppBodyState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
+class _AppBodyState extends State<AppBody> {
+  RaisedButton raisedButton(int i, [int length = 2]) {
+    return (RaisedButton(
+      onPressed: () {
+        setState(() {
+          questionNumber < (length - 1) ? questionNumber++ : questionNumber--;
+        });
+        print('$i');
+      },
+      child: Text('answer $i'),
+    ));
   }
+
+  int questionNumber = 0;
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    var questions = ['What \'s your favorite color?', 'What\'s your favorite animal?'];
+    return Column(
+      children: [
+        Text(questions.elementAt(questionNumber)),
+        raisedButton(1, questions.length),
+        raisedButton(2),
+        raisedButton(3),
+      ],
     );
   }
 }
